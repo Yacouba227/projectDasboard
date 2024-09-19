3b52ada649fda428c48d0d4374c78899df04ce3d
Yacouba227 <halidouyacouba999@gmail.com>
2024-09-19 17:20:56 +0100

    Detail pages

--- Js/Script.js
+++ Js/Script.js
@@ -1,8 +1,41 @@
 let Navigation = document.querySelector(".Navigation");
 let Main = document.querySelector(".Main");
 let Toggle = document.querySelector(".Toggle");
+const afficher = document.querySelectorAll("#afficher");
+const contenu_masquer  = document.querySelector(".contenu-masquer");
+const close = document.getElementById('close')
 
 Toggle.onclick = () => {
     Navigation.classList.toggle("active");
     Main.classList.toggle("active");
 }
+
+/* voirSuivi.forEach(element => {
+    element.addEventListener('click', (e) =>{
+      nomDossier = element.parentElement.parentElement.parentElement.firstElementChild.textContent;
+      console.log('====================================');
+      console.log(nomDossier);
+      localStorage.setItem('nom', JSON.stringify(nomDossier))
+      console.log('====================================');
+    })
+  }); */
+
+afficher.forEach(element => {
+   element.addEventListener("click", (e)=>{
+    /* console.log('====================================');
+    console.log(e);
+    console.log('===================================='); */
+    if (e.target) {
+        contenu_masquer.style.display = 'block';
+    }
+})
+});
+
+close.addEventListener("click", (e)=>{
+   /*  console.log('====================================');
+    console.log(e);
+    console.log('===================================='); */
+    if (e.target) {
+        contenu_masquer.style.display = 'none';
+    }
+})
